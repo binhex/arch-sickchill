64ef19a6e0be58c9c3416d6b9573f72c6031f283
binhex <megalith01@gmail.com>
2022-07-28 10:19:20 +0100

    incorrect path to site-packages

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/dumb-init /bin/bash
 
 # get path to site-packages
-install_path=$(python -m site --user-site)
+install_path=$(python3 -c "import sysconfig; print(sysconfig.get_path('purelib'))")
 
 # run app from python system
 python3 "${install_path}/SickChill.py" --config /config/config.ini --datadir /config
